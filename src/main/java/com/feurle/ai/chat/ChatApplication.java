f998987bb295155e52cc28db0cb3b49a6ff21bcd
Daniel Feurle <dfeurle@yandex.com>
2026-02-21 11:21:57 +0100

    Fix warning

--- src/main/java/com/feurle/ai/chat/ChatApplication.java
+++ src/main/java/com/feurle/ai/chat/ChatApplication.java
@@ -2,7 +2,6 @@ package com.feurle.ai.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class ChatApplication {
